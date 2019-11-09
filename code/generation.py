675fa77bb9ab95700ef135ac2d5cd9114af37fd3
Jan Gołda <jan.golda@gmail.com>
2019-11-09 23:24:41 +0100

    Reorganized code

---
+++ code/generation.py
@@ -0,0 +1,42 @@
+from typing import List
+
+import numpy as np
+
+
+def random_points_plane(n: int, corner1: np.ndarray, corner2: np.ndarray) -> np.ndarray:
+    x = np.random.uniform(size=(n, 1), low=min(corner1[0], corner2[0]), high=max(corner1[0], corner2[0]))
+    y = np.random.uniform(size=(n, 1), low=min(corner1[1], corner2[1]), high=max(corner1[1], corner2[1]))
+    return np.hstack((x, y))
+
+
+def random_points_circle(n: int, radius: float, center: np.ndarray) -> np.ndarray:
+    angles = np.random.uniform(low=0, high=2 * np.pi, size=n)
+    points = np.full((n, 2), radius, dtype='d')
+    points[:, 0] *= np.cos(angles)
+    points[:, 1] *= np.sin(angles)
+    points[:, 0] += center[0]
+    points[:, 1] += center[1]
+    return points
+
+
+def random_points_segment(n: int, point1: np.ndarray, point2: np.ndarray) -> np.ndarray:
+    t = np.random.uniform(size=(n, 1))
+    return (1 - t) * point1 + t * point2
+
+
+def random_points_polygon(n: int, points: List[np.ndarray]) -> np.ndarray:
+    return np.vstack([
+        random_points_segment(n // len(points), points[i-1], points[i])
+        for i in range(len(points))
+    ])
+
+
+def random_points_weird(n_edge: int, n_diagonal: int,
+                        p1: np.ndarray, p2: np.ndarray, p3: np.ndarray, p4: np.ndarray) -> np.ndarray:
+    return np.vstack([
+        [p1], [p2], [p3], [p4],
+        random_points_segment(n_edge, p1, p2),
+        random_points_segment(n_edge, p2, p3),
+        random_points_segment(n_diagonal, p1, p3),
+        random_points_segment(n_diagonal, p2, p4),
+    ])
---
+++ code/visualization.py
@@ -0,0 +1,49 @@
+from typing import List, Any, Tuple
+
+import numpy as np
+import matplotlib as mpl
+import matplotlib.pyplot as plt
+from matplotlib.collections import LineCollection
+from matplotlib.colors import to_rgba
+
+
+mpl.rcParams['animation.html'] = 'jshtml'
+
+
+def plot_points(points: np.ndarray, annotations: List[str] = [], keep_aspect: bool = True, **kwargs):
+
+    # points
+    if len(points):
+        plt.scatter(*zip(*points), **kwargs)
+
+    # aspect ratio
+    if keep_aspect:
+        plt.axis('equal')
+
+    # annotations
+    for p, text in zip(points, annotations):
+        plt.annotate(text, p, (5, 5), textcoords='offset pixels')
+
+
+def plot_segments(segments: List[Tuple[np.ndarray, np.ndarray]], colors: Any = None, zorder: int = 1, **kwargs):
+
+    # handle colors conversion
+    if isinstance(colors, list):
+        colors = [to_rgba(c) for c in colors]
+    elif colors is not None:
+        colors = [to_rgba(colors)] * len(segments)
+
+    # plot segments using collection for better performance
+    plt.gca().add_collection(LineCollection(segments, colors=colors, zorder=zorder, **kwargs))
+
+
+def plot_chain(points: np.ndarray, closed: bool = False, **kwargs):
+
+    # create segments
+    segments = [(points[i - 1], points[i]) for i in range(1, len(points))]
+
+    # close loop if requested
+    if closed:
+        segments.append((points[-1], points[0]))
+
+    plot_segments(segments, **kwargs)
