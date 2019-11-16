331ea763cc52ec51903ae85f08a9ebb86ea5c28f
Jan Gołda <jan.golda@gmail.com>
2019-11-16 22:58:16 +0100

    Renamed code to src to prevent name conflicts with python 'code' package

--- src/geometry/convex_hull.py
+++ src/geometry/convex_hull.py
@@ -1,56 +1,11 @@
-import copy
 from functools import cmp_to_key, partial
 
 import numpy as np
-import matplotlib.pyplot as plt
-from matplotlib.animation import FuncAnimation
 
-from code.geometry import orient, dist2
+from src.geometry import orient, dist2
 
 
-class ConvexHullAnimation:
-    """ Creates step-by-step animation of convex hull creation. """
-
-    def __init__(self, points, frames):
-        self.points = points
-        self.frames = frames
-
-        # init plot
-        self.fig, self.ax = plt.subplots()
-        self.ax.set_aspect('equal')
-
-        self.scatter_points = self.ax.scatter(*zip(*self.points), color='b', s=3)
-        self.scatter_hull = self.ax.scatter([], [], color='r')
-        self.scatter_active = self.ax.scatter([], [], color='g')
-        self.line_hull = self.ax.plot([], [], color='r')[0]
-
-        # init animation
-        self.animation = FuncAnimation(self.fig, self.animation_step, self.frames, self.animation_init, blit=True)
-
-    def animation_init(self):
-        self.scatter_hull.set_offsets(np.empty((0, 2)))
-        self.scatter_active.set_offsets(np.empty((0, 2)))
-        self.line_hull.set_data([], [])
-        return self.scatter_hull, self.scatter_active, self.line_hull
-
-    def animation_step(self, frame):
-        hull, point = frame
-
-        if point is None:
-            self.scatter_active.set_offsets(np.empty((0, 2)))
-        else:
-            self.scatter_active.set_offsets([point])
-
-        if hull is None:
-            return self.scatter_active,
-
-        self.scatter_hull.set_offsets(hull)
-        self.line_hull.set_data(*zip(*hull))
-
-        return self.scatter_hull, self.scatter_active, self.line_hull
-
-
-def graham_generator(points: np.ndarray, epsilon: float = 1e-10):
+def graham(points: np.ndarray, epsilon: float = 1e-10):
 
     # find first point (with lowest y then x) and remove it from points
     i0 = min(enumerate(points), key=lambda x: (x[1][1], x[1][0]))[0]
@@ -79,8 +34,6 @@ def graham_generator(points: np.ndarray, epsilon: float = 1e-10):
     # main loop
     i = 0
     while i < len(points):
-        yield copy.copy(hull), points[i]
-
         # if its on the right
         if len(hull) < 2 or orient(hull[-2], hull[-1], points[i]) > epsilon:
             hull.append(points[i])
@@ -89,11 +42,10 @@ def graham_generator(points: np.ndarray, epsilon: float = 1e-10):
         else:
             hull.pop()
 
-    yield hull + hull[:1], None
     return hull
 
 
-def jarvis_generator(points: np.ndarray, epsilon: float = 1e-10):
+def jarvis(points: np.ndarray, epsilon: float = 1e-10):
 
     # find first point (with lowest y then x)
     p0 = points[min(enumerate(points), key=lambda x: (x[1][1], x[1][0]))[0]]
@@ -103,12 +55,10 @@ def jarvis_generator(points: np.ndarray, epsilon: float = 1e-10):
 
     # main loop
     for i in range(len(points)):
-        yield copy.copy(hull), None
 
         # find right most point relative to last in hull
         best = None
         for p in points:
-            yield None, p
 
             # if there is no best
             if best is None:
@@ -127,5 +77,5 @@ def jarvis_generator(points: np.ndarray, epsilon: float = 1e-10):
         # add found point to hull
         hull.append(best)
 
-    yield hull + hull[:1], None
     return hull
+
